1175f1b0600b8592dc3e503a83167cf2401b9898
Rolena Williams <rolenawilliams@Rolenas-MacBook-Air.local>
2026-03-25 18:37:48 -0400

    animation added.

--- lib/welcomescreen.dart
+++ lib/welcomescreen.dart
@@ -1,9 +1,38 @@
 import 'package:flutter/material.dart';
-class WelcomeScreen extends StatelessWidget {
-  final String name; // 🆕 receives name from previous screen
+class WelcomeScreen extends StatefulWidget {
+  final String name;
 
   const WelcomeScreen({super.key, required this.name});
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
 
+  late AnimationController _controller; 
+  late Animation<double> _fadeAnimation;
+  
+  @override
+  void initState() {
+    super.initState();
+  
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    _fadeAnimation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeIn,
+    );
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,10 +41,12 @@ class WelcomeScreen extends StatelessWidget {
         backgroundColor: Colors.purple,
       ),
       body: Center(
-        // 🆕 display dynamic name
+        child: FadeTransition(
+        opacity: _fadeAnimation,
         child: Text(
-          'Welcome, $name!',
+          'Welcome, ${widget.name}!',
           style: const TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+          ),
         ),
       ),
     );
